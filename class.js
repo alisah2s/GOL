3bc2ff1779975b4a4182a038f896a6dd574239e9
alisah2s <alisa.h2.s@tumo.org>
2021-07-02 17:35:21 +0400

    avelacac fayler

--- class.js
+++ class.js
@@ -1,20 +1,3 @@
-class Grass {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.multiply = 0;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-
-    }
 
     chooseCell(character) {
         var found = [];
